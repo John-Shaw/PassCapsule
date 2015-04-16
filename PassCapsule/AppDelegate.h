5e40cdd12600b9319bd0f27b21324a6a6b74647e
John Shaw <bard2093@gmail.com>
2015-04-16 16:57:12 +0800

    Initial Commit

---
+++ PassCapsule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PassCapsule
+//
+//  Created by 邵建勇 on 15/4/16.
+//  Copyright (c) 2015年 John Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PassCapsule/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  PassCapsule
+//
+//  Created by 邵建勇 on 15/4/16.
+//  Copyright (c) 2015年 John Shaw. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
