7d74f9c8f6f08356432ee5ca60160f3104bd91c0
John Shaw <bard2093@gmail.com>
2015-05-20 19:13:47 +0800

    设计登陆界面。

---
+++ PassCapsule/PCLoginVC.h
@@ -0,0 +1,13 @@
+//
+//  PCLoginVC.h
+//  PassCapsule
+//
+//  Created by 邵建勇 on 15/5/20.
+//  Copyright (c) 2015年 John Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCLoginVC : UIViewController
+
+@end
---
+++ PassCapsule/PCLoginVC.m
@@ -0,0 +1,33 @@
+//
+//  PCLoginVC.m
+//  PassCapsule
+//
+//  Created by 邵建勇 on 15/5/20.
+//  Copyright (c) 2015年 John Shaw. All rights reserved.
+//
+
+#import "PCLoginVC.h"
+
+@interface PCLoginVC ()
+
+@end
+
+@implementation PCLoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
